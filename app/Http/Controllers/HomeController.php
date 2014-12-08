8df7942ee40019415c95e894cdde771c9959814e
Oliver Lumby <me@oliverlumby.com>
2014-12-08 20:24:31 +0000

    Remove unused controllers and templates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,25 +2,13 @@
 
 class HomeController extends Controller {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller renders your application's "dashboard" for users that
-	| are authenticated. Of course, you are free to change or remove the
-	| controller as you wish. It is just here to get your app started!
-	|
-	*/
-
 	/**
 	 * Create a new controller instance.
 	 *
-	 * @return void
+	 * @return \App\Http\Controllers\HomeController
 	 */
 	public function __construct()
 	{
-		$this->middleware('auth');
 	}
 
 	/**
