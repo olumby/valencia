90b88e4f8fa1de368e97bd0110707fb2533dfd36
Oliver Lumby <me@oliverlumby.com>
2014-12-10 20:23:19 +0000

    Move map Javascript to jQuery plugin

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+(function($){
+	$.openDataManager = function(options){
+		var base = this;
+
+		base.init = function(){
+			base.options = $.extend({},$.openDataManager.defaultOptions, options);
+
+			base.loadMap();
+		};
+
+		base.loadMap = function(){
+			var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
+				attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+			});
+
+			var map = L.map(base.options.mapElementId, {
+				scrollWheelZoom: false,
+				center: [39.466667, -0.383333],
+				zoom: 13
+			});
+
+			map.addLayer(layer);
+		};
+
+		base.init();
+	};
+
+	$.openDataManager.defaultOptions = {
+		mapType: "SimpleGeoPoints"
+	};
+
+	$.fn.openDataManager = function(MapType, options){
+		return this.each(function(){
+			(new $.openDataManager(this, MapType, options));
+		});
+	};
+})(jQuery);
